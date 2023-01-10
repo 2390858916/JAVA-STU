85ce0f4baa30b560d8e39c9ab52ec1bd366b30c9
zhong <2390858916@QQ.COM>
2023-01-10 19:51:12 +0800

    arrays

---
+++ src/ArraysDemo/Demo.java
@@ -0,0 +1,14 @@
+package ArraysDemo;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        int[] arr = {24,62,84,13};
+        System.out.println("Before sorting" + Arrays.toString(arr));
+
+        //from small to large
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
