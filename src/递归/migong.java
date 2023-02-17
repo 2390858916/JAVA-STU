dc7983c0f923dbf16e15571d66a821d10d02bfec
zhong <239085916@qq.com>
2023-02-17 21:59:58 +0800

    递归

--- src/递归/migong.java
+++ src/递归/migong.java
@@ -30,7 +30,7 @@ public class migong {
             for (int anInt : ints) {
                 System.out.print(anInt + " ");
             }
-            System.out.println(" ");
+            System.out.println("  ");
         }
     }
 }
