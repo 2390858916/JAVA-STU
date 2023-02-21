c18092de5681b6ec28f9207646b78f3a52eb81f1
zhong <2390858916@qq.com>
2023-02-21 22:23:26 +0800

    递归4

---
+++ src/方法重载overload/重载2.java
@@ -0,0 +1,12 @@
+package 方法重载overload;
+
+public class 重载2 {
+    public static void main(String[] args) {
+
+    }
+}
+class demo{
+    public int function(int a,int b){
+        return a+b;
+    }
+}
