1280ae8405c082c7d2f1f7b8ebbc46263ed8e1cd
2390858916@qq.com <2390858916@qq.com>
2023-04-08 23:49:31 +0800

    可变参数 2023/4/5 extends

---
+++ src/方法重载override/main.java
@@ -0,0 +1,9 @@
+package ∑Ω∑®÷ÿ‘ÿoverride;
+
+public class main {
+    public static void main(String[] args) {
+        Pserson p = new Pserson("jack",10);
+        System.out.println(p.say());
+
+    }
+}
